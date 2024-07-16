190be3d02ae952f18f253964fe6189f957168c35
Laurent Rineau <laurent.rineau@cgal.org>
2024-07-16 23:27:24 +0200

    fix SELinux images

--- fix-selinux.sh
+++ fix-selinux.sh
@@ -2,4 +2,4 @@
 
 shopt -s nullglob
 
-exec chcon -v -R --type container_file_t db-init* db-data* LocalSettings.php caddy-data* config update-context *.sh
+exec chcon -v -R --type container_file_t db-init* db-data* images*/ LocalSettings.php caddy-data* config update-context *.sh
