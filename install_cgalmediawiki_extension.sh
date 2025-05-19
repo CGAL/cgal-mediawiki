5b49185ff3a0fe0c2af42bed28549510c511f50e
Nicolas Saillant <saillantnicolas01@gmail.com>
2025-05-19 09:34:14 +0200

    update WikiMarkdown repository URL

--- install_cgalmediawiki_extension.sh
+++ install_cgalmediawiki_extension.sh
@@ -5,8 +5,7 @@ echo "Mediawiki version is $MW_VERSION (branch $BRANCH)"
 REPO_BASE="https://gerrit.wikimedia.org/r/mediawiki/extensions"
 EXTENSIONS="VisualEditor ConfirmAccount Interwiki Renameuser UserMerge MagicNoCache Math ParserFunctions SyntaxHighlight_GeSHi UrlGetParameters DiscussionThreading DismissableSiteNotice"
 
-WIKIMARKDOWN_REPO="https://github.com/lrineau/WikiMarkdown"
-WIKIMARKDOWN_BRANCH=fix-for-mw1.42
+WIKIMARKDOWN_REPO="https://github.com/SaillantNicolas/WikiMarkdown.git"
 PARSEDOWN_EXTENDED_REPO="https://github.com/BenjaminHoegh/ParsedownExtended"
 
 pushd /var/www/html
@@ -29,7 +28,10 @@ if [ -d "WikiMarkdown" ]; then
     rm -rf "WikiMarkdown"
 fi
 
-git clone --depth 1 -b "$WIKIMARKDOWN_BRANCH" "$WIKIMARKDOWN_REPO" WikiMarkdown
+if [ -d "$WIKIMARKDOWN_REPO" ]; then
+    rm -rf "$WIKIMARKDOWN_REPO"
+fi
+git clone --depth 1 "$WIKIMARKDOWN_REPO" WikiMarkdown
 
 if [ -d "WikiMarkdown/vendor/parsedown-extended" ]; then
     rm -rf "WikiMarkdown/vendor/parsedown-extended"
